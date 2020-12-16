e68b23137126da30721ea9dae35366ac669bb140
yashbonde <bonde.yash97@gmail.com>
2020-12-17 00:51:36 +0530

    make tests more fixed and add GPT code

--- gpt_gt.py
+++ gpt_gt.py
@@ -2,8 +2,13 @@
 import re
 import numpy as np
 from types import SimpleNamespace
-from graph_trans import GraphEncoder, TransformerConvBlock
+
+from torch.nn.functional import cross_entropy
+from graph_trans import GraphEncoder, TransformerConvBlock, ModelConfig, init_weights
 rnd = np.random.randn
+
+import torch
+from torch import nn, functional as F
 
 # open a sample text file
 with open("sample.txt", "r", encoding = "utf-8") as f:
@@ -13,10 +18,50 @@ with open("sample.txt", "r", encoding = "utf-8") as f:
     print(vocab)
     data = np.array([vocab[x] for x in data])
 
-# decalre the config
-config = SimpleNamespace(
+# ---- model ---- #
+class GPT(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.wte = nn.Embedding(config.vocab_size, config.n_embd)
+        self.wpe = nn.Embedding(config.spv_size, config.n_embd)
+        self.emb_drop = nn.Dropout(p = 0.1)
+        self.body = nn.Sequential(*[
+            TransformerConvBlock(config) for _ in range(config.n_layer)
+        ])
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size)
+        self.apply(init_weights)
+
+    @property
+    def num_params(self):
+        return sum(dict((p.data_ptr(), p.numel()) for p in self.parameters()).values())
+    
+    def forward(self, input_ids, sp_tokens, edge_index, edge_attr, targets = None):
+        x = self.wte(input_ids) + self.wpe(sp_tokens)
+        x = self.emb_drop(x)
+        x, _, _ = self.body((x, edge_index, edge_attr))
+        logits = self.lm_head(x)
+        out = [logits]
+        if targets is not None:
+            loss = cross_entropy(logits, targets)
+            out = out + [loss]
+        return out
+
+# declare the config
+config = ModelConfig(
     vocab_size = len(vocab),
     n_embd = 18,
+    maxlen = 40,
+    spv_size = 40,
+    n_layer = 2,
+    n_head = 2
 )
+gpt_model = GPT(config)
 
+# clip data
+data = data[:-(len(data) % config.maxlen)]
+data = data.reshape(-1, config.maxlen)
+
+print("*** DATASET:", data.shape)
+print("*** GPT MODEL:", gpt_model.num_params)
 
